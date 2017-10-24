b6f4511306c26c1efaa54081d152136a5032e01f
wang542 <wang542@indiana.edu>
2017-10-24 08:50:40 -0400

    GUI added

---
+++ src/application/ElevatorPanelController.java
@@ -0,0 +1,75 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ElevatorPanelController {
+	
+	
+	@FXML
+	private RadioButton FloorOne;
+	@FXML
+	private RadioButton FloorTwo;
+	@FXML
+	private RadioButton FloorThree;
+	@FXML
+	private RadioButton FloorFour;
+	@FXML
+	private RadioButton FloorFive;
+	@FXML
+	private RadioButton FloorSix;
+	@FXML
+	private RadioButton FloorSeven;
+	@FXML
+	private RadioButton FloorEight;
+	@FXML
+	private RadioButton FloorNine;
+	@FXML
+	private RadioButton FloorTen;
+	@FXML
+	private Button FloorUP;
+	@FXML
+	private Button FloorDOWN;
+	@FXML
+	private Button One;
+	@FXML
+	private Button Two;
+	@FXML
+	private Button Three;
+	@FXML
+	private Button Four;
+	@FXML
+	private Button Five;
+	@FXML
+	private Button Six;
+	@FXML
+	private Button Seven;
+	@FXML
+	private Button Eight;
+	@FXML
+	private Button Nine;
+	@FXML
+	private Button Ten;
+	@FXML
+	private Button Open;
+	@FXML
+	private Button Close;
+	@FXML
+	private TextField ElevatorFloor;
+	@FXML
+	private TextField ElevatorGoing;
+	@FXML
+	private TextField ElevatorSchedule;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
